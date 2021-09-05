bb446d1c91843425e84adfe1f97b6c6163b80826
Mark Flegg <mark.flegg@structuredpracticejournal.com>
2021-09-05 08:02:49 -0400

    upgraded to latest capacitor

---
+++ apps/markflegg-website/capacitor.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="@capacitor/local-notifications" />
+/// <reference types="@capacitor/push-notifications" />
+/// <reference types="@capacitor/splash-screen" />
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.capacitorjs.app.testapp',
+  appName: 'markflegg-website',
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+    },
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#CE0B7C',
+    },
+    PushNotifications: {
+      presentationOptions: ['alert', 'sound'],
+    },
+  },
+};
+
+export default config;
