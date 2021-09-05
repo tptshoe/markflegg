f8105b0a330f859f803097e15791bcb911a6a3e6
Mark Flegg <mark.flegg@structuredpracticejournal.com>
2021-09-05 07:42:51 -0400

    Created markflegg-website app

---
+++ apps/markflegg-website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'markflegg-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
