8e205cd6aa1c48a86585c098030bcfb4a2421c42
Mark Flegg <mark.flegg@structuredpracticejournal.com>
2021-09-06 08:13:06 -0400

    #2 attempt to trigger e2e test

--- apps/markflegg-website-e2e/src/integration/app.spec.ts
+++ apps/markflegg-website-e2e/src/integration/app.spec.ts
@@ -10,4 +10,12 @@ describe('markflegg-website', () => {
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains(`Mark Flegg's Website`);
   });
+
+  it('should display testing message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains(`This line should affect testing`);
+  });
 });
